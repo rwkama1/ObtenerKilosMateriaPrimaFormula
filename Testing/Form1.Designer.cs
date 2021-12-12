700f25d7fd2a31b688973dce370d3c655f8944cf
rwkama1 <rwkamandriw@gmail.com>
2021-12-12 19:27:52 -0300

    1

---
+++ Testing/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.Formula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MateriaPrima = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Porcentaje = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kilos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Formula,
+            this.MateriaPrima,
+            this.Porcentaje,
+            this.Kilos});
+            this.dgvDatos.Location = new System.Drawing.Point(12, 61);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.RowHeadersVisible = false;
+            this.dgvDatos.Size = new System.Drawing.Size(776, 337);
+            this.dgvDatos.TabIndex = 1;
+            // 
+            // Formula
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle5.Format = "N0";
+            dataGridViewCellStyle5.NullValue = null;
+            this.Formula.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Formula.HeaderText = "Formula";
+            this.Formula.Name = "Formula";
+            this.Formula.ReadOnly = true;
+            // 
+            // MateriaPrima
+            // 
+            dataGridViewCellStyle6.NullValue = null;
+            this.MateriaPrima.DefaultCellStyle = dataGridViewCellStyle6;
+            this.MateriaPrima.HeaderText = "Materia Prima";
+            this.MateriaPrima.Name = "MateriaPrima";
+            this.MateriaPrima.ReadOnly = true;
+            // 
+            // Porcentaje
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle7.Format = "N0";
+            dataGridViewCellStyle7.NullValue = null;
+            this.Porcentaje.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Porcentaje.HeaderText = "Porcentaje";
+            this.Porcentaje.Name = "Porcentaje";
+            this.Porcentaje.ReadOnly = true;
+            // 
+            // Kilos
+            // 
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Kilos.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Kilos.HeaderText = "Kilos";
+            this.Kilos.Name = "Kilos";
+            this.Kilos.ReadOnly = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dtp);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(597, 49);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtrar";
+            this.groupBox1.Validating += new System.ComponentModel.CancelEventHandler(this.groupBox1_Validating);
+            // 
+            // dtp
+            // 
+            this.dtp.Location = new System.Drawing.Point(17, 16);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(200, 20);
+            this.dtp.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(713, 415);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(233, 16);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(461, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "formula vendida";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(631, 32);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Formula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MateriaPrima;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Porcentaje;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kilos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ Testing/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvDatos.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvDatos.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvDatos.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            List<LineaDocumento> listalineas = lineasDocumento();
+            CDocumento doc = new CDocumento(listalineas, new DateTime(2021, 11, 01));
+            foreach (LineaDocumento lineaDocumento in doc.lineadoc)
+            {
+               dgvDatos.Rows.Add($"Producto: {lineaDocumento.producto.codigo} ", "Formula: " + lineaDocumento.formula.codigo, "Kilos: " + lineaDocumento.kilos);
+              
+                foreach (Compuesto compuesto in lineaDocumento.formula.listacompuesto)
+                {
+
+                    dgvDatos.Rows.Add(lineaDocumento.formula.codigo, compuesto.MateriaPrima.codigonombre, compuesto.porcentaje, lineaDocumento.kilos);
+
+
+
+                }
+
+
+                int indice = dgvDatos.Rows.Add("", "", "", "asd");
+                dgvDatos.Rows[indice].DefaultCellStyle.Font = new Font(dgvDatos.Font, FontStyle.Bold);
+            }
+
+
+
+        }
+        private List<LineaDocumento> lineasDocumento()
+        {
+            Producto producto1 = new Producto(64454);
+            Producto producto2 = new Producto(755645);
+            Producto producto3 = new Producto(879978);
+            Producto producto4 = new Producto(756989);
+            Producto producto5 = new Producto(685456);
+
+            CMateriaPrima materiaprima = new CMateriaPrima("6554 MateriaPrima1");
+            CMateriaPrima materiaprima2 = new CMateriaPrima("752 MateriaPrima2");
+            CMateriaPrima materiaprima3 = new CMateriaPrima("568 MateriaPrima3");
+            CMateriaPrima materiaprima4 = new CMateriaPrima("742 MateriaPrima4");
+            CMateriaPrima materiaprima5 = new CMateriaPrima("567 MateriaPrima5");
+            CMateriaPrima materiaprima6 = new CMateriaPrima("415 MateriaPrima6");
+            CMateriaPrima materiaprima7 = new CMateriaPrima("568 MateriaPrima7");
+            CMateriaPrima materiaprima8 = new CMateriaPrima("875 MateriaPrima8");
+            CMateriaPrima materiaprima9 = new CMateriaPrima("985 MateriaPrima9");
+            CMateriaPrima materiaprima10 = new CMateriaPrima("852 MateriaPrima10");
+
+
+            List<Compuesto> listacompuesto1 = new List<Compuesto>
+            {
+             new Compuesto(materiaprima,90),
+            new Compuesto(materiaprima2, 10)
+            };
+            List<Compuesto> listacompuesto2 = new List<Compuesto>
+            {
+             new Compuesto(materiaprima3,80),
+            new Compuesto(materiaprima4, 20)
+            };
+            List<Compuesto> listacompuesto3 = new List<Compuesto>
+            {
+             new Compuesto(materiaprima5,70),
+            new Compuesto(materiaprima6, 30)
+            };
+            List<Compuesto> listacompuesto4 = new List<Compuesto>
+            {
+             new Compuesto(materiaprima7,60),
+            new Compuesto(materiaprima8, 40)
+            };
+            List<Compuesto> listacompuesto5 = new List<Compuesto>
+            {
+             new Compuesto(materiaprima9,50),
+            new Compuesto(materiaprima10, 50)
+            };
+
+            CFormula formula1 = new CFormula(485, listacompuesto1);
+            CFormula formula2 = new CFormula(495, listacompuesto2);
+            CFormula formula3 = new CFormula(458, listacompuesto3);
+            CFormula formula4 = new CFormula(526, listacompuesto4);
+            CFormula formula5 = new CFormula(885, listacompuesto5);
+
+            LineaDocumento lineaDocumento = new LineaDocumento(formula1, producto1, 10000);
+            LineaDocumento lineaDocumento2 = new LineaDocumento(formula2, producto2, 20000);
+            LineaDocumento lineaDocumento3 = new LineaDocumento(formula3, producto3, 30000);
+            LineaDocumento lineaDocumento4 = new LineaDocumento(formula4, producto4, 40000);
+            LineaDocumento lineaDocumento5 = new LineaDocumento(formula5, producto5, 50000);
+
+            List<LineaDocumento> listalineasdocumento = new List<LineaDocumento>
+            {
+                lineaDocumento,lineaDocumento2,lineaDocumento3,lineaDocumento4,lineaDocumento5
+            };
+
+            return listalineasdocumento;
+
+        }
+
+        struct Producto
+        {
+            public int codigo;
+
+
+            public Producto(int codigo)
+            {
+                this.codigo = codigo;
+
+            }
+
+        }
+        struct CMateriaPrima
+        {
+            public string codigonombre;
+            public CMateriaPrima(string codigonombre)
+            {
+                this.codigonombre = codigonombre;
+
+            }
+
+        }
+        struct Compuesto
+        {
+
+            public CMateriaPrima MateriaPrima;
+            public int porcentaje;
+
+            public Compuesto(CMateriaPrima materiaPrima, int porcentaje)
+            {
+                this.MateriaPrima = materiaPrima;
+                this.porcentaje = porcentaje;
+            }
+        }
+        struct CFormula
+        {
+            public int codigo;
+            public List<Compuesto> listacompuesto;
+
+            public CFormula(int codigo, List<Compuesto> listacompuesto)
+            {
+                this.codigo = codigo;
+                this.listacompuesto = listacompuesto;
+            }
+        }
+
+        struct LineaDocumento
+        {
+            public CFormula formula;
+            public Producto producto;
+            public int kilos;
+
+            public LineaDocumento(CFormula formula, Producto producto, int kilos)
+            {
+                this.formula = formula;
+                this.producto = producto;
+                this.kilos = kilos;
+            }
+        }
+        struct CDocumento
+        {
+
+            public List<LineaDocumento> lineadoc;
+
+            public DateTime fecha;
+
+            public CDocumento(List<LineaDocumento> lineadoc, DateTime fecha)
+            {
+                this.lineadoc = lineadoc;
+                this.fecha = fecha;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void groupBox1_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+    }
+}
